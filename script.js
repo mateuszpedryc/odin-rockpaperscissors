13509a432b7ec87fddf6727f8aee49272d9fff64
mateusz.pedryc <mateusz.pedryc@gmail.com>
2023-02-13 12:47:23 +0100

    Create function to check who wins

--- script.js
+++ script.js
@@ -6,17 +6,16 @@
 //announce the winner.
 
 //Get player choice:
-
 let getPlayerChoice = prompt('What is your choice?');
 
-    //console.log(getPlayerChoice);
-
-
+//Function to make player input same all the time -> 'Rock', 'Paper', or 'Scissors':
+//function playerChoiceCapitalize(playerChoice) {
+  //  return playerChoice.toLowerCase();
+//}
 
 //Function to generate computer choice:
-
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choices = ['rock', 'paper', 'scissors'];
     let computerNumber = Math.floor(Math.random() * choices.length);
 
     let computerChoice = choices[computerNumber];
@@ -24,4 +23,57 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-    //console.log(getComputerChoice());
+let playerSelection = getPlayerChoice.toLowerCase();
+let computerSelection = getComputerChoice();
+
+//Function to play single round:
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It\'s a Tie!';
+    }
+    else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            return 'Computer Wins!';
+        }
+        else {
+            return 'Player Wins!';
+        }
+    }
+    else if (playerSelection = 'paper') {
+        if (computerSelection = 'scissors') {
+            return 'Computer Wins!';
+        }
+        else {
+            return 'Player Wins!';
+        }
+    }
+    else if (playerSelection = 'scissors') {
+        if (computerSelection = 'rock') {
+            return 'Computer Wins!';
+        }
+        else {
+            return 'Player Wins!';
+        }
+    }
+}
+
+
+
+
+console.log('You chose ' + playerSelection + ' and computer chose ' + computerSelection);
+console.log(playRound(playerSelection,computerSelection));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
